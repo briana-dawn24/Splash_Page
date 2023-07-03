a1e1178e8368c601c78d9e3c5458641d2300a189
briana-dawn24 <138332720+briana-dawn24@users.noreply.github.com>
2023-07-03 22:43:09 +0000

    Fixed enter and back button.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// JavaScript code to handle button click event
+// JavaScript code to handle enter button.
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function() 
 {
     window.location.href = this.href;
 });
 // Javascript code to handle back button
-var btn = document.querySelector(".goback-button");
+var btn2 = document.querySelector(".goback-button");
 btn.addEventListener("click", function() {
   goBack();
 });
