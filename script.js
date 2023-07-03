e1ed2a25924cccf3782c889cf4be62e1dd17f7bd
briana-dawn24 <138332720+briana-dawn24@users.noreply.github.com>
2023-07-03 22:50:10 +0000

    Not needed code?

--- script.js
+++ script.js
@@ -3,14 +3,3 @@ document.getElementById("btn").addEventListener("click", function()
 {
     window.location.href = this.href;
 });
-
-// Javascript code to handle back button
-  document.querySelector("btnback").addEventListener("click", function()
-  {
-    goBack();
-  });
-  
-  function goBack()
-  {
-    history.back();
-  }
