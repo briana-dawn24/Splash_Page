558e11a959ff1197e6d58574165f5e91d82c6dff
briana-dawn24 <138332720+briana-dawn24@users.noreply.github.com>
2023-07-03 22:45:04 +0000

    Fixing buttons again.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // JavaScript code to handle enter button.
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function() 
+document.getElementById("btn").addEventListener("click", function()
 {
     window.location.href = this.href;
 });
-// Javascript code to handle back button
-var btn2 = document.querySelector(".goback-button");
-btn.addEventListener("click", function() {
-  goBack();
-});
 
-function goBack() {
-  history.back();
-}
+// Javascript code to handle back button
+  document.querySelector(".goback-button").addEventListener("click", function()
+  {
+    goBack();
+  });
+  
+  function goBack()
+  {
+    history.back();
+  }
